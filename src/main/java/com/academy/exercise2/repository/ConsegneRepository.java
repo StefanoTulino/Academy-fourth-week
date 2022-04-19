40d60a749c7059a8e3d9a656fc3373c77feb885a
Stefano Tulino <56756052+StefanoTulino@users.noreply.github.com>
2022-04-19 09:08:59 +0200

    Delete comments
    Delete comments

--- src/main/java/com/academy/exercise2/repository/ConsegneRepository.java
+++ src/main/java/com/academy/exercise2/repository/ConsegneRepository.java
@@ -9,8 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ConsegneRepository extends JpaRepository<Consegne,String> {
 
-    /*
-    @Query(value = "{numberProduct:?0}",countQuery = "città")
-    Integer getDeliveriesCountByCity(String id,String city);
-     */
+    
 }
